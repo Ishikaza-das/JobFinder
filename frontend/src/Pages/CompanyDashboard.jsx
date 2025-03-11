fe60337413b30a4cdc1b5628841f66696a95a030
Ritesh Das <riteshdas8652@gmail.com>
2025-03-11 16:25:05 +0530

    Openings Counter

--- frontend/src/Pages/CompanyDashboard.jsx
+++ frontend/src/Pages/CompanyDashboard.jsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
 
-export default function CompanyDashboard() {
+const CompanyDashboard = () => {
+
+  const [totalJob, setTotalJob] = useState({totalJobs:0});
+  const fetchTotalJobs = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/openings/stats`,{ withCredentials: true });
+      setTotalJob(response.data.stats)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchTotalJobs();
+    const interval = setInterval(fetchTotalJobs, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <>
-      
+     <div className="container px-2 py-8 mx-auto">
+        <div className="grid grid-cols-3 gap-6 h-52">
+            <div className="bg-gradient-to-br from-blue-800 to-blue-300 rounded-lg shadow-lg text-white">
+              <h1 className="font-bold m-4 text-2xl">Total Job Posted</h1>
+              <span className="font-semibold m-4 text-2xl">{totalJob.totalJobs}</span>
+            </div>
+            {/* Application viewed */}
+            <div className="bg-gradient-to-br from-blue-800 to-blue-300 rounded-lg shadow-lg text-white">
+              <h1 className="font-bold m-4 text-2xl">Application viewed</h1>
+              <span className="font-semibold m-4 text-2xl">Count</span>
+            </div>
+            {/* Application Recived */}
+            <div className="bg-gradient-to-br from-blue-800 to-blue-300 rounded-lg shadow-lg text-white">
+              <h1 className="font-bold m-4 text-2xl">Application Recived</h1>
+              <span className="font-semibold m-4 text-2xl">Count</span>
+            </div>
+        </div>
+      </div> 
     </>
   )
 }
+
+export default CompanyDashboard
